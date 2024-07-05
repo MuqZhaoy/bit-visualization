0763cd52a34ecdc084fce16ee4e39a9ec4730557
JiEunim <294290328@qq.com>
2024-07-05 23:29:50 +0800

    添加地图可视化

---
+++ data/2-mbti-worldmap/mbtimap2/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <MainContent></MainContent>
+  </div>
+</template>
+
+<script>
+import MainContent from './components/MainContent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MainContent,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ data/2-mbti-worldmap/mbtimap2/src/components/MainContent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="MainContent">
+        <Choropleth></Choropleth>
+    </div>
+</template>
+
+<script>
+import Choropleth from './Choropleth.vue'; // 导入 Choropleth 组件
+export default {
+    name: 'MainContent',
+    components: {
+        Choropleth // 注册 Choropleth 组件
+    }
+}
+</script>
+
+<style>
+
+</style>
